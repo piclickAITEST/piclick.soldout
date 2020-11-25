e721fa2c5cab3a90c60068b98b3bd32a85ea663f
NoMoreBuild <woolimlee.dev@gmail.com>
2020-11-25 18:28:00 +0900

    update: 스크롤 이벤트 사용하지 않는 기능 삭제

--- Front/scroll.js
+++ Front/scroll.js
@@ -2,11 +2,6 @@
 function handleScroll() {
     var height = $(document).innerHeight() - $(window).innerHeight();
     var currPosition = $(window).scrollTop();
-
-    // 스크롤이 특정 위치에 도달 했을 경우에만 표시
-    // height * 0.5 < currPosition ? $('.btn-list').removeAttr("hidden") : $('.btn-list').attr("hidden", "")
-
-    // 스크롤 위치가 50% 달성 시 무조건 표시
     if (height * 0.5 < currPosition) { 
         $('.btn-list').removeAttr("hidden")
     }
@@ -17,9 +12,3 @@ $(window).scroll($.throttle(150, function () {
         handleScroll()
     })
 )
-
-// 쓰로틀 제거 스크롤 이벤트
-// $(window).scroll(function () {
-//         handleScroll()
-//     }
-// )
